73880de6a04b0e81cfc31e633703e4efad565ea2
jhs <jsp7406@naver.com>
2023-08-04 13:31:07 +0900

    feat: "todolist_javascript 수정"

--- js/todolist.js
+++ js/todolist.js
@@ -1,4 +1,24 @@
 const addTodoButtonOnClickHandle = () => {
+    // spread 문법
+    // const testObj = {
+    //     name: "정혜성",
+    //     age: 10
+    // }
+
+    // console.log(testObj);
+
+    // const testArray = [1,2,3,4,5];
+    // console.log(testArray);
+    // const testArray2 = [...testArray, 6,7,8];
+    // console.log(testArray2);
+
+    // const testObj2 = {
+    //     ...testObj,
+    //     address: "부산",
+    //     name: "ㅈㅎㅅ"
+    // }
+    // console.log(testObj2);
+
     generateTodoObj();
 }
 
@@ -8,13 +28,22 @@ const addTodoOnKeyUpHandle = (event) => {
     }
 }
 
+const checkedOnChangeHandle = (target) => {
+    TodoListService.getInstance().setCompletStatus(target.value, target.checked);
+}
+
+const deleteTodoOnClickHandle = (target) => {
+    TodoListService.getInstance().removeTodo(target.value);
+}
+
 const generateTodoObj = () => {
     const todoContent = document.querySelector(".todolist-header-items .text-input").value;
     
     const todoObj = {
         id: 0,
         todoContent: todoContent,
-        createDate: DateUtils.toStringByFormatting(new Date())
+        createDate: DateUtils.toStringByFormatting(new Date()),
+        completStatus: false
     };
 
     TodoListService.getInstance().addTodo(todoObj);
@@ -42,7 +71,11 @@ class TodoListService {
 
     loadTodoList() {
         this.todoList = !!localStorage.getItem("todoList") ? JSON.parse(localStorage.getItem("todoList")) : new Array();
-        this.todoIndex = !!this.todoList[this.todoList.length - 1]?.id ? this.todoList[this.todoList.length - 1].id : 1;
+        this.todoIndex = !!this.todoList[this.todoList.length - 1]?.id ? this.todoList[this.todoList.length - 1].id + 1 : 1;
+    }
+
+    saveLocalStorage() {
+        localStorage.setItem("todoList", JSON.stringify(this.todoList));
     }
 
     addTodo(todoObj) {
@@ -53,8 +86,56 @@ class TodoListService {
         
         this.todoList.push(todo);
 
-        localStorage.setItem("todoList", this.todoList);
+        this.saveLocalStorage();
+
+        this.updateTodoList();
 
         this.todoIndex++;
+    }
+
+    setCompletStatus(id, status) {
+
+        this.todoList.forEach((todo, index) => {
+            if(todo.id === parseInt(id)) {
+               this.todoList[index].completStatus = status; 
+            }
+        });
+
+        this.saveLocalStorage();
+    }
+
+    removeTodo(id) {
+        this.todoList = this.todoList.filter(todo => {
+            return todo.id !== parseInt(id);
+        });
+
+        this.saveLocalStorage();
+        this.updateTodoList();
+    }
+
+    updateTodoList() {
+        const todoListMainContainer = document.querySelector(".todolist-main-container");
+
+        todoListMainContainer.innerHTML = this.todoList.map(todo => {
+            return `
+                <li class="todolist-items">
+                    <div class="item-left">
+                        <input type="checkbox" id="complet-chkbox${todo.id}" class="complet-chkboxs" 
+                        ${todo.completStatus ? "checked" : ""} value="${todo.id}" onchange="checkedOnChangeHandle(this);">
+                        <label for="complet-chkbox${todo.id}"></label>
+                    </div>
+                    <div class="item-center">
+                        <pre class="todolist-content">${todo.todoContent}</pre>
+                    </div>
+                    <div class="item-right">
+                        <p class="todolist-date">${todo.createDate}</p>
+                        <div class="todolist-item-buttons">
+                            <button class="btn btn-edit" value="${todo.id}" onclick="modifyTodoOnClickHandle(this);">수정</button>
+                            <button class="btn btn-remove" value="${todo.id}" onclick="deleteTodoOnClickHandle(this);">삭제</button>
+                        </div>
+                    </div>
+                </li>
+            `;
+        }).join("");
     }
 }
